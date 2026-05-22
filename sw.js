7c2f5b6952c9f6c012572678ef74216df6cfb302
duskomacak-png <duskomacak@gmail.com>
2026-05-22 23:44:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // Minimal cache reset service worker for CityStyle.app
-const CACHE_NAME = "citystyle-business-v30-hardreset";
+const CACHE_NAME = "citystyle-business-v31-top-icons-fix";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
