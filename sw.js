bf28d06ed935442fe6997dd13f2a346d9005aa36
duskomacak-png <duskomacak@gmail.com>
2026-05-26 18:19:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // Minimal cache reset service worker for CityStyle.app
-const CACHE_NAME = "citystyle-v138-iconsfix";
+const CACHE_NAME = "citystyle-v139-iconmini";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -32,7 +32,7 @@ self.addEventListener("push", (event) => {
       url: data.url || "salon/",
       badgeCount: data.badgeCount || 1
     },
-    tag: data.tag || "citystyle-v138-iconsfix",
+    tag: data.tag || "citystyle-v139-iconmini",
     renotify: true
   };
 
