067bb20d15b29eedf44c522c597e43df1d5d2495
duskomacak-png <duskomacak@gmail.com>
2026-06-05 19:25:02 +0200

    Add files via upload

--- assets/js/client.js
+++ assets/js/client.js
@@ -1953,7 +1953,7 @@ function renderShoeViewer() {
     viewer.id = "shoeViewer";
     document.body.appendChild(viewer);
   }
-  viewer.className = "shoe-viewer shoe-viewer-locked shoe-viewer-clean-card-v251 shoe-viewer-final-v254";
+  viewer.className = "shoe-viewer shoe-viewer-locked shoe-viewer-final-v255";
   viewer.classList.toggle("shoe-viewer-zoomed", !!csViewerState.zoomed);
   viewer.setAttribute("data-price", csProductPrice(product));
   const viewerMetaPrimary = csProductViewerMetaPrimary(product);
