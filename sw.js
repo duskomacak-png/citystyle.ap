833ff84daa2f15d26d6cd618f0824dd048933b1d
duskomacak-png <duskomacak@gmail.com>
2026-06-28 11:51:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // CityStyle v251 - shop perfect slika tri dugmeta
-const CACHE_NAME = "citystyle-v263_shop_mobile_button_green_border";
+const CACHE_NAME = "citystyle-v265_shop_viewer_x_center";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
